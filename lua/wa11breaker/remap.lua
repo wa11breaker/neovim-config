0475426cde10b26b9fad83843af3f4d0679bf448
Akshay <akshayasok1998@gmail.com>
2023-01-21 16:01:42 +0530

    feat: added tabs related remap

--- lua/wa11breaker/remap.lua
+++ lua/wa11breaker/remap.lua
@@ -1,16 +1,19 @@
 local map = vim.keymap.set
 local opts = { silent = true }
 
+-- Set space as leader
 vim.g.mapleader = " "
 
-map("n", "j", "gj")
-map("n", "k", "gk")
+-- Map
+map("n", "<Leader>w", "<C-w>")
+
+-- Better up/down
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Better escape using jk in insert and terminal mode
 map("i", "jk", "<ESC>")
 map("t", "jk", "<C-\\><C-n>")
-
-map("n", "<Leader>w", "<C-w>")
 
 -- Move lines
 map("v", "J", ":m '>+1<CR>gv=gv")
@@ -31,6 +34,13 @@ map("i", "<C-l>", "<Right>", opts)
 -- Join lines without moving the curser to the end
 map("n", "J", "mzJ`z")
 
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- Clear search with <esc>
+map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
 -- Half page jumping and curser stays in the middle
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
@@ -45,6 +55,14 @@ map("x", "<leader>p", [["_dP]])
 -- Deleting to void registry
 map({ "n", "v" }, "<leader>d", [["_d]])
 
--- terminal
+-- Terminal
 map({ "n", "t" }, "<leader>t", "<Cmd>ToggleTerm<CR>")
 map({ "n", "t" }, "<C-t>", "<Cmd>ToggleTerm<CR>")
+
+-- Tabs
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+map("n", "<leader><tab>q", "<cmd>tabclose<cr>", { desc = "Close Tab" })
